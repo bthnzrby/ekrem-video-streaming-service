71bc14bf09219ff6913bf002c3ddbc239d366ba9
bthnzrby <xbthnzrbyx@gmail.com>
2024-05-19 07:32:44 +0300

    devam

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <Routes>
+        {/* Bekir Sıdkı Sezgin */}
         <Route
           path="/Bekir-Sidki-Sezgin-1.Misra"
           element={
@@ -183,6 +184,205 @@ function App() {
             <Component
               content="Bekir Sıdkı Sezgin 8. Terennüm"
               videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%208.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-Nat-i-Mevlana"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin Nat-ı Mevlana"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Nat-%C4%B1%20Mevlana-BSS.mp4"
+            />
+          }
+        />
+        {/* Kani Karaca */}
+        <Route
+          path="/Kani-Karaca-1.Misra"
+          element={
+            <Component
+              content="Kâni Karaca 1. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%201.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-2.Misra"
+          element={
+            <Component
+              content="Kâni Karaca 2. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%202.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-3.Misra"
+          element={
+            <Component
+              content="Kâni Karaca 3. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%203.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-4.Misra"
+          element={
+            <Component
+              content="Kâni Karaca 4. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%204.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-5.Misra"
+          element={
+            <Component
+              content="Kâni Karaca 5. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%205.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-6.Misra"
+          element={
+            <Component
+              content="Kâni Karaca 6. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%206.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-7.Misra"
+          element={
+            <Component
+              content="Kâni Karaca 7. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%207.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-8.Misra"
+          element={
+            <Component
+              content="Kâni Karaca 8. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%208.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-9.Misra"
+          element={
+            <Component
+              content="Kâni Karaca 9. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%209.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-10.Misra"
+          element={
+            <Component
+              content="Kâni Karaca 10. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%2010.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-11.Misra"
+          element={
+            <Component
+              content="Kâni Karaca 11. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%2011.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-12.Misra"
+          element={
+            <Component
+              content="Kâni Karaca 12. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%2012.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-1.Terennum"
+          element={
+            <Component
+              content="Kâni Karaca 1. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%201.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-2.Terennum"
+          element={
+            <Component
+              content="Kâni Karaca 2. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%202.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-3.Terennum"
+          element={
+            <Component
+              content="Kâni Karaca 3. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%203.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-4.Terennum"
+          element={
+            <Component
+              content="Kâni Karaca 4. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%204.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-5.Terennum"
+          element={
+            <Component
+              content="Kâni Karaca 5. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%205.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-6.Terennum"
+          element={
+            <Component
+              content="Kâni Karaca 6. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%206.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-7.Terennum"
+          element={
+            <Component
+              content="Kâni Karaca 7. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%207.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Kani-Karaca-8.Terennum"
+          element={
+            <Component
+              content="Kâni Karaca 8. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Ka%CC%82ni%20Karaca%208.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Rast-Naat-Kani-Karaca"
+          element={
+            <Component
+              content="Rast Naat Kâni Karaca"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/05%20-%20Rast%20Naat-Kani%20Karaca%20Kalan.mp4"
             />
           }
         />
