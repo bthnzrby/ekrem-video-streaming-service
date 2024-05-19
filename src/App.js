7a6bd3d20c97e82d5b6f50769d1e33a71af27400
bthnzrby <xbthnzrbyx@gmail.com>
2024-05-19 07:01:39 +0300

    Bekir Sıdkı Sezgin tamam

--- src/App.js
+++ src/App.js
@@ -24,6 +24,168 @@ function App() {
             />
           }
         />
+        <Route
+          path="/Bekir-Sidki-Sezgin-3.Misra"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 3. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%203.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-4.Misra"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 4. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%204.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-5.Misra"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 5. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%205.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-6.Misra"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 6. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%206.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-7.Misra"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 7. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%207.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-8.Misra"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 8. Misra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%208.M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-9.Misra"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 9. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%209.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-10.Misra"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 10. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%2010.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-11.Misra"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 11. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%2011.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-12.Misra"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 12. Mısra"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%2012.%20M%C4%B1sra.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-1.Terennum"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 1. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%201.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-2.Terennum"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 2. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%202.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-3.Terennum"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 3. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%203.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-4.Terennum"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 4. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%204.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-5.Terennum"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 5. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%205.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-6.Terennum"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 6. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%206.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-7.Terennum"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 7. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%207.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
+        <Route
+          path="/Bekir-Sidki-Sezgin-8.Terennum"
+          element={
+            <Component
+              content="Bekir Sıdkı Sezgin 8. Terennüm"
+              videopath="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%208.%20Terennu%CC%88m.mp4"
+            />
+          }
+        />
       </Routes>
     </div>
   );
