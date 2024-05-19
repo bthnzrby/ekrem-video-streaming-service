f054d32ccf07ade1a20cfcf939baf910fe0546d2
bthnzrby <xbthnzrbyx@gmail.com>
2024-05-19 04:13:46 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -7,14 +7,9 @@ function App() {
 
       <video width="400px" height="200px" controls>
         <source
-          src="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%206.%20Terennu%CC%88m.mp4"
+          src="https://d3rklpiaxtqmrs.cloudfront.net/Bekir%20S%C4%B1dk%C4%B1%20Sezgin%208.%20M%C4%B1sra.mp4"
           // type="video/mp4"
         />
-
-        {/* <source
-          src="https://d3rklpiaxtqmrs.cloudfront.net/Bekir-Sıdkı-Sezgin-8.-Mısra.webm"
-          type="video/webm"
-        ></source> */}
       </video>
     </div>
   );
