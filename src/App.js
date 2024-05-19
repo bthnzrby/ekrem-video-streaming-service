a881eb331c49060f459aee8ddb3d7c2e999ff1de
bthnzrby <xbthnzrbyx@gmail.com>
2024-05-19 03:16:09 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
       <h2> Bekir Sıdkı Sezgin 8. Mısra</h2>
 
       <video width="400px" height="200px" controls>
-        <source
-          src="https://d3rklpiaxtqmrs.cloudfront.net/Bekir-S%C4%B1dk%C4%B1-Sezgin-8.-M%C4%B1sra.wav"
-          type="video/mp4"
-        />
+        <source src="https://d3rklpiaxtqmrs.cloudfront.net/Bekir-S%C4%B1dk%C4%B1-Sezgin-8.-M%C4%B1sra.wav" />
       </video>
     </div>
   );
